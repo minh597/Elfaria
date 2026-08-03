cbc7643ec4e811aeddf3cfdd115b6a4c67278572
minh597 <minhdangtran1012@gmail.com>
2026-08-03 11:17:21 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,21 +1,2 @@
-local oldHttpGet = game.HttpGet
-local oldHttpGetAsync = game.HttpGetAsync
-local oldHttpPost = game.HttpPost
-local oldHttpPostAsync = game.HttpPostAsync
 
-game.HttpGet = function(...)
-    error("HttpGet blocked")
-end
-
-game.HttpGetAsync = function(...)
-    error("HttpGetAsync blocked")
-end
-
-game.HttpPost = function(...)
-    error("HttpPost blocked")
-end
-
-game.HttpPostAsync = function(...)
-    error("HttpPostAsync blocked")
-end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/minh597/Elfaria/refs/heads/main/klaffyTDX.lua"))()
