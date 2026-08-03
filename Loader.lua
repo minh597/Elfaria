c5570bbbb3467e9d1498a723140845bf17d82f04
minh597 <minhdangtran1012@gmail.com>
2026-08-03 11:00:53 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,10 +3,10 @@ local games = {
     [10375105020] = "https://raw.githubusercontent.com/minh597/Elfaria/refs/heads/main/SpeedBoatTsunami.lua"
 }
 
-local url = games[game.PlaceId]
+local url = games[game.GameId]
 
 if url then
     loadstring(game:HttpGet(url))()
 else
-    warn("Game not supported: " .. game.PlaceId)
+    warn("Game not supported: " .. tostring(game.GameId))
 end
